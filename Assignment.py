3175101b65a8caa5df08b930d1c2949f4ac6f7fa
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2021-02-16 00:02:47 +0600

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,59 @@
+print("Hello World")
+
+x=1
+y=2
+print(x+y)
+
+sum = x+y
+print(sum)
+
+name = "Abu"
+name2 = "taher"
+print(name , name2)
+
+type(name)
+
+multiplication = 4*3
+print(multiplication)
+
+add = 11+24
+print(add)
+
+sub = 67-8
+print(sub)
+
+Exponentiation = 3**10
+print(Exponentiation)
+
+True_Division = 3/3
+print(True_Division)
+
+Floor_Division = 7//4
+print(Floor_Division)
+
+remainder = 15%7
+print(remainder)
+
+print('Welcome', 'to', 'Python!')
+
+print('Welcome\nto\n\nPython!')
+
+print('this is a longer string, so we \
+ split it over two lines')
+
+print('Sum is', 7 + 3)
+
+print('int(5.2)', 'truncates 5.2 to', int(5.2))
+
+print('Display "hi" in quotes')
+
+number1 = int(input('Enter first integer: '))
+number2 = int(input('Enter second integer: '))
+number3 = int(input('Enter third integer: '))
+
+x=2
+print('x =', x)
+
+x=2
+y=3
+print((x + y), '=', (y + x))
